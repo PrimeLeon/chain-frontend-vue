47879e792e6f3a94de94716acdce0895f09c457f
PrimeLeon <PrimeLeon@qq.com>
2020-10-27 20:08:36 +0800

    feat(views): add some module files.

--- src/views/commuser/create.vue
+++ src/views/commuser/create.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>111</div>
+  <div>welcome to this module</div>
 </template>
 
 <script>
-export default {
-
-}
+  // import axios from 'axios'
+  // export default {
+  //   name: 'app',
+  //   result: '',
+  //   created () {
+  //     const url = `https://test.youbaobao.xyz:18081`;
+  //     const api = `/book/home/v2?openId=1234`;
+  //     axios.get(url + api, response => {
+  //       console.log(response);
+  //     })
+  //   }
+  // }
 </script>
 
 <style lang="scss" scoped>
